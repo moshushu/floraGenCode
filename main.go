2c03c983e730a71f468a0f9c22266831cedad05a
moshushu <2434130846@qq.com>
2022-12-15 22:14:41 +0800

    文件写入

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/xuri/excelize/v2"
 )
 
-var student = `package %s \n func %s() {}`
+var student = "package %s \n func %s() {}"
 
 func main() {
 	f, err := excelize.OpenFile("./实体.xlsx")
@@ -14,21 +15,50 @@ func main() {
 		fmt.Println("file open field,err:", err)
 		return
 	}
-	defer func() {
-		if err := f.Close(); err != nil {
-			fmt.Println("file close field,err:", err)
-		}
-	}()
+	defer f.Close()
 	rows, err := f.GetRows("Sheet1")
 	if err != nil {
 		fmt.Println("file get rows field,err:", err)
 		return
 	}
-	for _, row := range rows {
-		// for _, r := range row {
-		// // 	fmt.Printf("%s,", r)
-		// }
-		// fmt.Println()
-		fmt.Printf(student, row[1], row[3])
+	for index, row := range rows {
+		if index == 0 {
+			data := fmt.Sprintf(student, row[1], row[3])
+			createGoFile(row[1], data)
+			fmt.Println(data)
+		}
 	}
 }
+
+// createGoFile 创建Go文件
+func createGoFile(name, data string) {
+	fileName := "./student/" + name + ".go"
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("createGoFile filed err:", err, "name is ", name)
+		return
+	}
+	defer file.Close()
+	num, err := file.WriteString(data)
+	if err != nil {
+		fmt.Println("write err", err)
+		return
+	}
+	fmt.Println("num", num)
+}
+
+// func writeGoFile(name, data string) {
+// 	name = "./student/StuentInfo.txt"
+// 	data = "去你妈的"
+// 	file, err := os.OpenFile(name, os.O_WRONLY, 0777)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer file.Close()
+// 	buffer := bufio.NewWriter(file)
+// 	str, err := buffer.WriteString(data)
+// 	if err != nil {
+// 		log.Fatalln("writestring filed err:", err)
+// 	}
+// 	fmt.Println("str", str)
+// }
