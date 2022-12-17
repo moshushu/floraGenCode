f8c3e202dbdacb90fb1bfc9e212c9dc258f94f61
moshushu <2434130846@qq.com>
2022-12-17 22:59:14 +0800

    支持多个实体一起创建

--- main.go
+++ main.go
@@ -8,41 +8,38 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
+type nameAndData struct {
+	name string
+	data string
+}
+
+var (
+	entityName = make(map[string]string, 0)
+	entityData = make(map[string][][]string, 0)
+)
+
 func main() {
-	f, err := excelize.OpenFile("./实体.xlsx")
+	err := readXLSXFile("./实体.xlsx", "Sheet1")
 	if err != nil {
-		fmt.Println("file open field,err:", err)
-		return
+		panic(err)
 	}
-	defer f.Close()
-	rows, err := f.GetRows("Sheet1")
-	if err != nil {
-		fmt.Println("file get rows field,err:", err)
-		return
+	datas := splitTemplate()
+	for _, data := range datas {
+		createAndWriteGoFile(data)
 	}
-	var entity, entityName, entityDate, filedDate string
-	for _, row := range rows[1:] {
-		if row[0] != "" && row[1] != "" {
-			entity = row[0]
-			entityName = row[1]
-		}
-		filedDate += filedDate + "\n" + fmt.Sprintf(temp.FieldContent, row[3], row[4], row[3], row[3])
-	}
-	entityDate = fmt.Sprintf(temp.PackName, entityName) + fmt.Sprintf(temp.EntityContent, entityName, entity, entity, filedDate)
-	createGoFile(entityName, entityDate)
 }
 
 // createGoFile 创建Go文件
-func createGoFile(name, data string) {
-	fileName := "./student/" + name + ".go"
+func createAndWriteGoFile(data nameAndData) {
+	fileName := "./student/" + data.name + ".go"
 	file, err := os.Create(fileName)
 	if err != nil {
-		fmt.Println("createGoFile filed err:", err, "name is ", name)
+		fmt.Println("createGoFile filed err:", err, "name is ", data.name)
 		return
 	}
 	defer file.Close()
 
-	num, err := file.WriteString(data)
+	num, err := file.WriteString(data.data)
 	if err != nil {
 		fmt.Println("write err", err)
 		return
@@ -50,16 +47,51 @@ func createGoFile(name, data string) {
 	fmt.Println("num", num)
 }
 
-func readXLSXFile(name string) {
-	f, err := excelize.OpenFile("name")
+// readXLSXFile 读取xlsx文件内容
+func readXLSXFile(name, sheet string) error {
+	f, err := excelize.OpenFile(name)
 	if err != nil {
 		fmt.Println("file open field,err:", err)
-		return
+		return err
 	}
 	defer f.Close()
-	rows, err := f.GetRows("Sheet1")
+	rows, err := f.GetRows(sheet)
 	if err != nil {
 		fmt.Println("file get rows field,err:", err)
-		return
+		return err
 	}
+	var cn, en string
+	for _, row := range rows[1:] {
+		if cn != row[0] && en != row[1] {
+			cn = row[0]
+			en = row[1]
+			entityName[row[1]] = row[0]
+			entityData[row[1]] = append(entityData[row[1]], row[2:])
+		} else {
+			entityData[row[1]] = append(entityData[row[1]], row[2:])
+		}
+	}
+	fmt.Println("entityData", entityData)
+	return err
+}
+
+// splitTemplate 拼接需要写入的内容
+func splitTemplate() []nameAndData {
+	n := []nameAndData{}
+	for name, datas := range entityData {
+		entityDate := ""
+		filedDate := ""
+		nameCn := entityName[name]
+		for _, data := range datas {
+			filedDate += fmt.Sprintf(temp.FieldContent, data[1], temp.FieldType[data[2]], data[0], data[0]) + "\n"
+		}
+		entityDate = fmt.Sprintf(temp.PackName, name) +
+			fmt.Sprintf(temp.EntityContent, name, nameCn, nameCn, filedDate)
+
+		n = append(n, nameAndData{
+			name: name,
+			data: entityDate,
+		})
+	}
+	return n
 }
